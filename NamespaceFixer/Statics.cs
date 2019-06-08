5fad471ed81164cf1477a138e7721e514d24da1b
Sébastien VILLEMAIN <villemain.s@gmail.com>
2019-06-08 16:35:09 +0200

    to respect the configuration of the author

--- NamespaceFixer/Statics.cs
+++ NamespaceFixer/Statics.cs
@@ -2,19 +2,20 @@
 
 namespace NamespaceFixer
 {
-    internal static class Statics
-    {
-        public const string ProjectFileExtension = "csproj";
-    }
+  internal static class Statics
+  {
+    public const string CsProjectFileExtension = "csproj";
+    public const string VbProjectFileExtension1 = "vbproj";
+  }
 
-    internal static class Guids
-    {
-        public static readonly Guid NamespaceFixerCmdSet = new Guid("{19492BCB-32B3-4EC3-8826-D67CD5526653}");
-        public const string NamespaceFixerPackage = "3C7C5ABE-82AC-4A37-B077-0FF60E8B1FD3";
-    }
+  internal static class Guids
+  {
+    public static readonly Guid NamespaceFixerCmdSet = new Guid("{19492BCB-32B3-4EC3-8826-D67CD5526653}");
+    public const string NamespaceFixerPackage = "3C7C5ABE-82AC-4A37-B077-0FF60E8B1FD3";
+  }
 
-    internal static class Ids
-    {
-        public const int CmdIdAdjustNamespace = 0x2001;
-    }
+  internal static class Ids
+  {
+    public const int CmdIdAdjustNamespace = 0x2001;
+  }
 }
