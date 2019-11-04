bd98c8173d0e8a655997dc6d51d8a206fd4c07d3
pauer24 <pau.cervello@gmail.com>
2019-11-04 10:37:37 +0100

    Fixing typo

--- NamespaceFixer/NamespaceBuilder/NamespaceSections.cs
+++ NamespaceFixer/NamespaceBuilder/NamespaceSections.cs
@@ -5,7 +5,7 @@
         public const string SolutionName = "{solutionName}";
         public const string ProjectName = "{projectName}";
         public const string ProjectRootNamespace = "{projectRootNamespace}";
-        public const string ProjectToSolutionPhysicalPath = "{projectToSolutionPhisicalPath}";
+        public const string ProjectToSolutionPhysicalPath = "{projectToSolutionPhysicalPath}";
         public const string ProjectToSolutionVirtualPath = "{projectToSolutionVirtualPath}";
         public const string FileToProjectPath = "{fileToProjectPath}";
     }
