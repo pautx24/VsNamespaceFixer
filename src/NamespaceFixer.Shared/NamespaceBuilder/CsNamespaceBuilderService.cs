3c0f1e3e205d0f39c8cb98607a5ebd944a770939
nb.duong <nb.duong@outlook.com>
2022-10-14 00:17:37 +0700

    #44 Namespace duplication when using file scoped namespaces
    issue: https://github.com/pautx24/VsNamespaceFixer/issues/44

--- src/NamespaceFixer.Shared/NamespaceBuilder/CsNamespaceBuilderService.cs
+++ src/NamespaceFixer.Shared/NamespaceBuilder/CsNamespaceBuilderService.cs
@@ -12,11 +12,10 @@ namespace NamespaceFixer.NamespaceBuilder
         {
         }
 
-        protected override Match FindNamespaceMatch(string fileContent) => 
-            Regex.Match(fileContent, @"[\r\n|\r|\n]?namespace\s(.+)[\r\n|\r|\n]*{");
-        
+        protected override Match FindNamespaceMatch(string fileContent) =>
+            Regex.Match(fileContent, @"[\r\n|\r|\n]?namespace\s(.+)[\r\n|\r|\n]*[{|;]");
 
-        protected override MatchCollection FindUsingMatches(string fileContent) => 
+        protected override MatchCollection FindUsingMatches(string fileContent) =>
             Regex.Matches(fileContent, @"\n?using\s(.+);");
 
         protected override string BuildNamespaceLine(string desiredNamespace) => "namespace " + desiredNamespace;
